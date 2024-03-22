aeac0378a7ec27ad8c7d0db2a69cdc2c4144c93c
Gus <cardona50368@gmail.com>
2024-03-22 13:04:18 -0500

    Update MoviesViewController and MoviesStrategy

--- MoviesApp/Details/DetailsViewController.swift
+++ MoviesApp/Details/DetailsViewController.swift
@@ -3,11 +3,9 @@
 //  Created by Gustavo Adolfo Cardona Quintero on 28/02/24.
 import UIKit
 import CoreData
-
 class DetailViewController: UIViewController {
     
     var detailView: DetailView? { self.view as? DetailView }
-    lazy var movieWebService = MoviesWebService()
     var movie: MovieDetail?
     var movieId: Int?
     var isFavoriteMovie = false
@@ -19,44 +17,72 @@ class DetailViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         getWebServiceDetail()
-    
         self.navigationItem.title = "Details"
-        
-        // Configurar el botón de favoritos
-        setupFavoriteButton()
-        
     }
     
     @objc private func addToFavorites() {
         guard let movieDetail = movie else { return }
-        // Crea una instancia de tu entidad Movie en el contexto de CoreData
-        let movieEntity = MoviesEntity(context: context)
-        // Configura las propiedades de la entidad
-        movieEntity.name_movie = movieDetail.title
-        movieEntity.id = Int64(movieDetail.id)
-        movieEntity.releaseDate_movie = movieDetail.release_date
-        movieEntity.posterPath_movie = movieDetail.poster_path
         
-        // Guarda el contexto para persistir los cambios en la base de datos
+        let fetchRequest: NSFetchRequest<MoviesEntity> = MoviesEntity.fetchRequest()
+        fetchRequest.predicate = NSPredicate(format: "id == %ld", movieDetail.id)
+        
         do {
+            let existingMovies = try context.fetch(fetchRequest)
+            
+            if let _ = existingMovies.first {
+                print("La película ya está en favoritos.")
+                return
+            }
+            
+
+            let movieEntity = MoviesEntity(context: context)
+            movieEntity.name_movie = movieDetail.title
+            movieEntity.id = Int64(movieDetail.id)
+            movieEntity.releaseDate_movie = movieDetail.release_date
+            movieEntity.posterPath_movie = movieDetail.poster_path
+            
             try context.save()
-            // Cambia la imagen del botón para mostrar que la película es favorita
-            navigationItem.rightBarButtonItem?.image = UIImage(systemName: "star.fill")
-            // Muestra algún mensaje o feedback al usuario
+            isFavoriteMovie = true
+            updateFavoriteButtonImage()
             print("La película ha sido agregada a favoritos.")
         } catch {
-            // Manejar el error aquí
-            print("Error al guardar la película en favoritos: \(error)")
+            print("Error al buscar o guardar la película en favoritos: \(error)")
         }
     }
 
+    @objc private func removeFromFavorites() {
+        guard let movieDetail = movie else { return }
+        
+        let fetchRequest: NSFetchRequest<MoviesEntity> = MoviesEntity.fetchRequest()
+        fetchRequest.predicate = NSPredicate(format: "id == %ld", movieDetail.id)
+        
+        do {
+            let existingMovies = try context.fetch(fetchRequest)
+            
+            if let existingMovie = existingMovies.first {
+                context.delete(existingMovie)
+                
+                try context.save()
+                isFavoriteMovie = false
+                updateFavoriteButtonImage()
+                print("La película ha sido eliminada de favoritos.")
+            } else {
+                print("La película no está en favoritos.")
+            }
+        } catch {
+            print("Error al buscar la película en favoritos: \(error)")
+        }
+    }
+    
     private func setupFavoriteButton() {
+        guard let movieDetail = movie else { return }
+        
         let favoritesButtonImage = createFavoriteIcon()
-        let favoritesButton = UIBarButtonItem(image: favoritesButtonImage, style: .plain, target: self, action: #selector(addToFavorites))
+        let favoritesButton = UIBarButtonItem(image: favoritesButtonImage, style: .plain, target: self, action: isFavoriteMovie ? #selector(removeFromFavorites) : #selector(addToFavorites))
         navigationItem.rightBarButtonItem = favoritesButton
         
         let leftButton = UIBarButtonItem(image: UIImage(systemName: "chevron.backward"),style: .plain, target: self, action: #selector(buttonToBack))
-            navigationItem.leftBarButtonItem = leftButton
+        navigationItem.leftBarButtonItem = leftButton
     }
     
     @objc private func buttonToBack() {
@@ -67,10 +93,25 @@ class DetailViewController: UIViewController {
         return UIImage(systemName: isFavoriteMovie ? "star.fill" : "star") ?? UIImage()
     }
 
-
     private func updateFavoriteButtonImage() {
         let image = createFavoriteIcon()
         navigationItem.rightBarButtonItem?.image = image
+        navigationItem.rightBarButtonItem?.action = isFavoriteMovie ? #selector(removeFromFavorites) : #selector(addToFavorites)
+    }
+    
+    private func checkFavoriteStatusAndUpdateButton() {
+        guard let movieId = self.movieId else { return }
+        
+        let fetchRequest: NSFetchRequest<MoviesEntity> = MoviesEntity.fetchRequest()
+        fetchRequest.predicate = NSPredicate(format: "id == %ld", movieId)
+        
+        do {
+            let existingMovies = try context.fetch(fetchRequest)
+            isFavoriteMovie = !existingMovies.isEmpty
+            setupFavoriteButton()
+        } catch {
+            print("Error al buscar la película en favoritos: \(error)")
+        }
     }
     
     private func getWebServiceDetail() {
@@ -81,6 +122,7 @@ class DetailViewController: UIViewController {
             self?.movie = movieDetail
             DispatchQueue.main.async {
                 self?.detailView?.dataInjection(fromModel: movieDetail)
+                self?.checkFavoriteStatusAndUpdateButton()
             }
         }
     }
